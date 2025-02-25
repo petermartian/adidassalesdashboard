0b161450dc8c70d61926a71967d01aa015d9961c
Peter Martian <petmartian@gmail.com>
2025-02-25 22:28:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ st.markdown('<style>div.block-container{padding-top:1rem;}<style>', unsafe_allow
 image = image.open('adidas-logo.jpg')
 
 col1, col2 = st.columns([0.1,0.9])
-with col1
+with col1:
       st.image(image, width = 100)
