27e58f8ce694f9b019d8c884036d0fbda82296c7
Peter Martian <petmartian@gmail.com>
2025-02-25 22:31:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import streamlit as st
 import pandas as pd
 import datetime
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as mp
 
 # reading the data from excel file
 df = pd.read_excel("Adidas.xlsx")
 st.set_page_config(page_title="Adidas Dashboard", page_icon=":bar_chart:", layout="wide")
 st.title("Adidas Dashboard")
 st.markdown('<style>div.block-container{padding-top:1rem;}<style>', unsafe_allow_html=True)
-image = image.open('adidas-logo.jpg')
+image_path = "adidas-logo.jpg"  # Ensure this file is in the same directory as your app.py or provide the full path
+
 
 col1, col2 = st.columns([0.1,0.9])
 with col1:
