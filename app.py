34cc55fae4a99b9816567167bb62fb3c4e434c70
Peter Martian <petmartian@gmail.com>
2025-02-25 22:33:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ st.set_page_config(page_title="Adidas Dashboard", page_icon=":bar_chart:", layou
 st.title("Adidas Dashboard")
 st.markdown('<style>div.block-container{padding-top:1rem;}<style>', unsafe_allow_html=True)
 image_path = "adidas-logo.jpg"  # Ensure this file is in the same directory as your app.py or provide the full path
+st.image(image_path, width=100)  # Display the image with a width of 100 pixels
 
-
-col1, col2 = st.columns([0.1,0.9])
-with col1:
-      st.image(image, width = 100)
+col1, col2 = st.columns([0.8, 0.9])
