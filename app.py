3f4dc200ab9d2f6a56d1a4255d7a2c94e9ab30c8
Peter Martian <petmartian@gmail.com>
2025-02-25 22:38:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as mp
 # reading the data from excel file
 df = pd.read_excel("Adidas.xlsx")
 st.set_page_config(page_title="Adidas Dashboard", page_icon=":bar_chart:", layout="wide")
-st.title("Adidas Dashboard")
 st.markdown('<style>div.block-container{padding-top:1rem;}<style>', unsafe_allow_html=True)
 image_path = "adidas-logo.jpg"  # Ensure this file is in the same directory as your app.py or provide the full path
 st.image(image_path, width=100)  # Display the image with a width of 100 pixels
